8d698f392647c0888ed9660f9954f7040426b1b9
Alexander Quintero <alex.quintero@gatech.edu>
2018-10-26 20:20:53 +0000

    defined state and actions space

---
+++ Game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, state):
+        self.state = state
+    
+    def step(self, next_state):
+        pass
--- student.py
+++ student.py
@@ -1,9 +1,20 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
+from game import Game
+from dqn import DQN
+from model import Model
+import numpy as np
 
 class StudentBot(BaseAgent):
     def initialize_agent(self):
-        pass
+        dqn = DQN()
+        self.model = Model(dqn)
+        self.game = Game()
+        self.epsilon = 0.05
     
     def get_output(self, packet):
+        state, reward, next_state = self.game.step(packet)
+        action = game.sample_action()
+        if np.random.random() > self.epsilon:
+            action = self.model.predict(game.state)
         
